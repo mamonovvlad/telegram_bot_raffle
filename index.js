57048b3bb5b924dbf51a884bc1a6c28ffd90b602
Влад <45657091+mamonovvlad@users.noreply.github.com>
2022-06-25 17:06:33 +0300

    fixed bot

--- index.js
+++ index.js
@@ -1,93 +1,68 @@
-let TelegramBot = require('node-telegram-bot-api');
+const {Scenes, session, Telegraf, Markup} = require('telegraf')
+const channel = "@channeltest0007"
+require('dotenv').config()
 
-// Устанавливаем токен, который выдавал нам бот.
-let token = '5333642362:AAHWgFsRXBTFyfnHj6vvZFXJTY8mTt4AwBo';
-
-// Включить опрос сервера
-let bot = new TelegramBot(token, {polling: true});
-
-let questions = [
-  {
-    title:'Первый Вопрос',
-    buttons: [
-      [{ text: 'Первый Вопрос', callback_data: '0_1' }],
-    ],
-  },
-  {
-    title:'Второй Вопрос',
-    buttons: [
-      [{ text: 'Второй Вопрос', callback_data: '1_1' }],
-    ],
-  },
-  {
-    title:'Третий Вопрос',
-    buttons: [
-      [{ text: 'Третий Вопрос', callback_data: '2_1' }],
-    ],
-  },
-];
-
-function getRandomQuestion(){
-  return questions[Math.floor(Math.random()*questions.length)];
-}
-
-function newQuestion(msg){
-  let arr = getRandomQuestion();
-  let text = arr.title;
-  let options = {
-    reply_markup: JSON.stringify({
-      inline_keyboard: arr.buttons,
-      parse_mode: 'Markdown'
-    })
-  };
-  chat = msg.hasOwnProperty('chat') ? "@channeltest0007" : msg.from.id;
-  bot.sendMessage(chat, text, options);
-}
-
-bot.onText(/\/start_test/, function (msg, match) {
-  newQuestion(msg);
-});
-
-bot.on('callback_query', function (msg) {
-  var answer = msg.data.split('_');
-  var index = answer[0];
-  var button = answer[1];
-  console.log(msg.from)
-  // if (questions[index].right_answer==button) {
-  //
-  //   bot.sendMessage(msg.from.id, 'Ответ верный ✅');
-  // } else {
-  //   bot.sendMessage(msg.from.id, 'Ответ неверный ❌');
-  // }
-
-  bot.answerCallbackQuery(msg.id, 'Вы выбрали: '+ msg.data, true);
-  newQuestion(msg);
-});
-
-
-// const {Scenes, session, Telegraf} = require('telegraf');
-//
-// require('dotenv').config()
-//
 // const bot = new Telegraf('5333642362:AAHWgFsRXBTFyfnHj6vvZFXJTY8mTt4AwBo')
-// // const index = new Telegraf(process.env.TOKEN)
-//
-//
-// const SceneGenerator = require('./src/scene')
-// const mongodb = require("./db");
-// const curScene = new SceneGenerator()
-//
-//
-// const stage = new Scenes.Stage([curScene.GenTextScene(), curScene.GenDateScene(), curScene.GenPublishScene(),])
-//
-// bot.use(session())
-// bot.use(stage.middleware())
-//
-// bot.start(async (ctx) => {
-//   // await ctx.scene.enter('text')
-//   console.log(curScene.GenPublishScene.buttons)
-// })
-//
-//
-// bot.launch().then()
+const bot = new Telegraf(process.env.TOKEN)
 
+
+const SceneGenerator = require('./src/scene')
+
+// const mongodb = require("./db");
+const curScene = new SceneGenerator()
+
+
+const stage = new Scenes.Stage([curScene.GenTextScene().text, curScene.GenDateScene().timer, curScene.GenPublishScene()])
+
+bot.use(session())
+bot.use(stage.middleware())
+
+
+bot.start(async (ctx) => {
+  await ctx.scene.enter('text')
+})
+
+
+//Опубликовать
+bot.action('btn--publish', async (ctx) => {
+  const description = curScene.GenTextScene().description
+  let timeFor = new Date(curScene.GenDateScene().dateChange).valueOf();
+  let now = new Date().getTime();
+  let sec = timeFor - now;
+  
+  ctx.reply('Готово')
+  let res = await ctx.telegram.sendMessage(channel, description,
+    Markup.inlineKeyboard([
+      [
+        Markup.button.callback(`Участвовать`, 'btn--participate',)
+      ]
+    ]))
+  
+  let opts = {
+    chat_id: channel,
+    message_id: res.message_id
+  }
+  
+  setTimeout(() => {
+    ctx.editMessageText(`${description}\n\nПобедитель(-и): @MamonovVlad`, opts)
+  }, sec)
+  
+})
+
+//Создать заново
+bot.action('btn--recreate', async (ctx) => {
+  try {
+    await ctx.scene.enter('text')
+  } catch (e) {
+    console.error(e)
+  }
+})
+
+//Участвовать
+bot.action('btn--participate', async (ctx) => {
+  ctx.answerCbQuery('Вы участвуите')
+  console.log(ctx.update.callback_query.from)
+  
+})
+
+bot.launch().then()
--- src/scene.js
+++ src/scene.js
@@ -1,9 +1,8 @@
-const {Markup, Composer, Scenes} = require('telegraf')
-const channel = "@channeltest0007"
-const mongodb = require('../db')
+const {Markup, Scenes} = require('telegraf')
 
+
+let dateChange;
 let description;
-let dateText;
 
 // let seconds;
 
@@ -11,6 +10,7 @@ class SceneGenerator {
   
   //Описание
   GenTextScene() {
+    
     const text = new Scenes.BaseScene('text')
     text.enter((ctx) => ctx.reply(`Привет ${ctx.from.first_name ? ctx.from.first_name : 'Незнакомец'}, напиши текст для розыгрыша `))
     text.on('message', async (ctx) => {
@@ -19,9 +19,9 @@ class SceneGenerator {
       if (description.length > 0 && description) {
         await ctx.scene.enter('timer')
       }
+      
     })
-    
-    return text
+    return {text, description}
   }
   
   //Время
@@ -31,14 +31,14 @@ class SceneGenerator {
       await ctx.reply("Введите дату в формате\n(месяц/число/год часы:минуты:секунды || 09/24/2022 09:25:32)")
     })
     timer.on('text', async (ctx) => {
-      dateText = ctx.message.text;
-      if (Date.parse(dateText) && dateText.length === 19) {
+      dateChange = ctx.message.text;
+      if (Date.parse(dateChange) && dateChange.length === 19) {
         await ctx.scene.enter('buttons')
       } else {
         ctx.reply('Ошибка')
       }
     })
-    return timer
+    return {timer, dateChange}
   }
   
   //Кнопки
@@ -48,101 +48,14 @@ class SceneGenerator {
       await ctx.reply('Опубликовать розыгрышь или отменить',
         Markup.inlineKeyboard([
           [
-            Markup.button.callback('Опубликовать', 'btn--publish'), Markup.button.callback('Предпросмотр', 'btn--preview'),
-          ],
-          [
-            Markup.button.callback('Отменить', 'btn--cansel'), Markup.button.callback('Создать заново', 'btn--recreate')
+            Markup.button.callback('Опубликовать', 'btn--publish'), Markup.button.callback('Создать заново', 'btn--recreate')
           ]
         ]))
       
     })
     
-    
-    //Опубликовать
-    buttons.action('btn--publish', async (ctx) => {
-      let timeFor = new Date(dateText).valueOf();
-      let now = new Date().getTime();
-      let sec = timeFor - now;
-      
-      ctx.reply('Готово')
-      
-      let res = await ctx.telegram.sendMessage(channel, description,
-        Markup.inlineKeyboard([
-          [
-            Markup.button.callback(`Участвовать`, 'btn--participate',)
-          ]
-        ]))
-      
-      let opts = {
-        chat_id: channel,
-        message_id: res.message_id
-      }
-      
-      ctx.getChatMenuButton({channel}, async (ctx) => {
-        await console.log(ctx)
-      })
-      
-      setTimeout(() => {
-        ctx.editMessageText(`${description}\n\nПобедитель(-и): @MamonovVlad`, opts)
-      }, sec)
-      
-      
-    })
-    
-    //Предпросмотр
-    buttons.action('btn--preview', async (ctx) => {
-      try {
-        ctx.reply(description, Markup.inlineKeyboard([
-          [
-            Markup.button.callback(`${description}\n\nПобедитель(-и): @MamonovVlad`, 'btn--participate')
-          ]
-        ]))
-        
-      } catch (e) {
-        console.error(e)
-      }
-    })
-    
-    //Отменить
-    buttons.action('btn--cansel', async (ctx) => {
-      try {
-        ctx.reply('Бот остановлен')
-      } catch (e) {
-        console.error(e)
-      }
-    })
-    
-    //Создать заново
-    buttons.action('btn--recreate', async (ctx) => {
-      try {
-        await ctx.scene.enter('text')
-      } catch (e) {
-        console.error(e)
-      }
-    })
-  
-    buttons.callbackQuery('exfc')
-    // buttons.on('callback_query', async (ctx) => {
-    //   // Участвовать
-    //   buttons.action('btn--participate', async (ctx) => {
-    //     await console.log(ctx.update.callback_query.from)
-    //     buttons.answerCallbackQuery('Вы выбрали: ')
-    //     // await JSON.stringify(fetchUsers(ctx.update.callback_query.from))
-    //   })
-    // })
     return buttons
   }
-  
 }
-
-
-
-// async function fetchUsers(data) {
-//   console.log(data)
-//   const filter = {id: data.id}
-//   const users = (await mongodb).db("telegram").collection('users');
-//   await users.updateOne(filter, {$set: data}, {upsert: true})
-//   return await users.findOne(filter)
-// }
 
 module.exports = SceneGenerator
