5e417c87ab12a521439e3a71e262a847b485c571
Влад <45657091+mamonovvlad@users.noreply.github.com>
2022-07-10 10:53:02 +0300

    add reply

--- index.js
+++ index.js
@@ -34,37 +34,31 @@ bot.start(async (ctx) => {
 //Участвовать
 bot.action('btn--participate', async (ctx) => {
   if ((await ctx.telegram.getChatMember(channel, ctx.update.callback_query.from.id)).status !== 'left') {
-    conn.connect(err => {
-      if (err) {
-        conn = mysql.createConnection(config)
-      }
-      const getUsersInfo = `INSERT INTO user (username, user_id)
-                            VALUES ('${ctx.update.callback_query.from.username}', '${ctx.update.callback_query.from.id}
+    const getUsersInfo = `INSERT INTO user (username, user_id)
+                          VALUES ('${ctx.update.callback_query.from.username}', '${ctx.update.callback_query.from.id}
                                     ')`;
-      
-      conn.query(getUsersInfo, (err, resultUsers, field) => {
-        const getMessage = `SELECT *
-                            FROM info_chat`
-        conn.query(getMessage, (err, resultMessage) => {
-          resultMessage.forEach(item => {
-            if (item.message_id === ctx.update.callback_query.message.message_id) {
-              if (resultUsers !== undefined) {
-                ctx.answerCbQuery('Вы участвуете 💸')
-                ctx.editMessageText(`${curScene.GenTextScene().description}`, Markup.inlineKeyboard([
-                  [
-                    Markup.button.callback(`Участвую! (${i += 1})`, 'btn--participate',)
-                  ]
-                ]), {
-                  chat_id: channel,
-                  message_id: ctx.update.callback_query.message.message_id
-                })
-              } else {
-                ctx.answerCbQuery('Вы уже участвуете в розыгрыше')
-              }
+    conn.query(getUsersInfo, (err, resultUsers, field) => {
+      const getMessage = `SELECT *
+                          FROM info_chat`
+      conn.query(getMessage, (err, resultMessage) => {
+        resultMessage.forEach(item => {
+          if (item.message_id === ctx.update.callback_query.message.message_id) {
+            if (resultUsers !== undefined) {
+              ctx.answerCbQuery('Вы участвуете 💸')
+              ctx.editMessageText(`${curScene.GenTextScene().description}`, Markup.inlineKeyboard([
+                [
+                  Markup.button.callback(`Участвую! (${i += 1})`, 'btn--participate',)
+                ]
+              ]), {
+                chat_id: channel,
+                message_id: ctx.update.callback_query.message.message_id
+              })
             } else {
-              ctx.answerCbQuery('Этот розыгрыше не актуален')
+              ctx.answerCbQuery('Вы уже участвуете в розыгрыше')
             }
-          })
+          } else {
+            ctx.answerCbQuery('Этот розыгрыше не актуален')
+          }
         })
       })
     })
@@ -84,31 +78,28 @@ bot.action('btn--publish', async (ctx) => {
             Markup.button.callback(`Участвую!`, 'btn--participate',)
           ]
         ]))
-      conn.connect(err => {
-          if (err) {
-            conn = mysql.createConnection(config)
-          }
-          let infoChat = `SELECT *
-                          FROM info_chat`
-          conn.query(infoChat, (err, result) => {
-            if (result.length > 0 && typeof result !== undefined) {
-              const updateDate = `UPDATE info_chat
-                                  SET date       = '${curScene.GenDateScene().dateChange}',
-                                      message_id = '${res.message_id}'`;
-              conn.query(updateDate, (err, result) => {
-                determineWinner(ctx)
-              })
-            } else {
-              const saveData = `INSERT INTO info_chat (date, message_id)
-                                VALUES ('${curScene.GenDateScene().dateChange}', '${res.message_id}')`;
-              conn.query(saveData, (err, result) => {
-                determineWinner(ctx)
-              })
-            }
-            
+      
+      let infoChat = `SELECT *
+                      FROM info_chat`
+      conn.query(infoChat, (err, result) => {
+        if (result.length > 0 && typeof result !== undefined) {
+          const updateDate = `UPDATE info_chat
+                              SET date       = '${curScene.GenDateScene().dateChange}',
+                                  message_id = '${res.message_id}'`;
+          conn.query(updateDate, (err, result) => {
+            console.log('Опубликовать SELECT')
+            determineWinner(ctx)
+          })
+        } else {
+          const saveData = `INSERT INTO info_chat (date, message_id)
+                            VALUES ('${curScene.GenDateScene().dateChange}', '${res.message_id}')`;
+          conn.query(saveData, (err, result) => {
+            console.log('Опубликовать INSERT')
+            determineWinner(ctx)
           })
         }
-      )
+        
+      })
     } else {
       ctx.reply('Текст не заполнен, запустите бота заново 🧐')
     }
@@ -117,65 +108,53 @@ bot.action('btn--publish', async (ctx) => {
 
 //Func
 //Определить победитель
-const determineWinner = (ctx) => {
-  conn.connect(err => {
-    if (err) {
-      conn = mysql.createConnection(config)
-    }
-    
-    const query = "SELECT * FROM info_chat"
-    conn.query(query, (err, result) => {
-      result.forEach(item => {
-        let drawDate = new Date(item.date)
-        let opts = {
-          chat_id: channel,
-          message_id: item.message_id
-        }
-        schedule.scheduleJob(drawDate, () => {
-          console.log('Запуск рандома')
-          runRandomizer(ctx, opts)
-        })
+async function determineWinner(ctx) {
+  console.log('determineWinner')
+  const query = "SELECT * FROM info_chat"
+  conn.query(query, (err, result) => {
+    result.forEach(item => {
+      let drawDate = new Date(item.date)
+      let opts = {
+        chat_id: channel,
+        message_id: item.message_id
+      }
+      ctx.reply('Начало времени')
+      schedule.scheduleJob(drawDate, () => {
+        ctx.reply('Время оконченно')
+        runRandomizer(ctx, opts)
       })
-      
-      
     })
+    
+    
   })
 }
 
 //Запустить  рандом
-const runRandomizer = (ctx, opts) => {
+function runRandomizer(ctx, opts) {
+  ctx.reply('Запустить  рандом')
   let winner
-  conn.connect(err => {
-    if (err) {
-      conn = mysql.createConnection(config)
-    }
-    const query = "SELECT * FROM user"
-    let res = []
-    conn.query(query, (err, result, field) => {
-      result.forEach(item => {
-        res.push(item.username)
-      })
-      //Выбираю победителя
-      if (typeof res !== undefined && res.length > 0) {
-        winner = res[Math.floor(Math.random() * res.length)]
-      } else {
-        winner = 'Победитель не определен'
-      }
-      ctx.editMessageText(`${curScene.GenTextScene().description}\n\nПобедитель: ${winner !== undefined ? winner : "Извините произошла ошибка"}`, opts)
-      drorDatabase()
+  const query = "SELECT * FROM user"
+  let res = []
+  conn.query(query, (err, result, field) => {
+    result.forEach(item => {
+      res.push(item.username)
     })
+    ctx.reply('Выбираю победителя')
+    //Выбираю победителя
+    if (typeof res !== undefined && res.length > 0) {
+      winner = res[Math.floor(Math.random() * res.length)]
+    } else {
+      winner = 'Победитель не определен'
+    }
+    ctx.editMessageText(`${curScene.GenTextScene().description}\n\nПобедитель: ${winner !== undefined ? winner : "Извините произошла ошибка"}`, opts)
+    drorDatabase()
   })
-  
-  
 }
 
-const drorDatabase = () => {
+function drorDatabase() {
   //Callback на очищения базы
   const query = 'DELETE FROM user'
   conn.query(query, (err, result, field) => {
-    if (err) {
-      console.log(err, 'drorDatabase')
-    }
   })
 }
 
